7af1d693d9c152e4722bbdf52c23e82d052eba2f
kailinc <kailinc@bu.edu>
2017-12-21 12:29:03 +0700

    Added css files
    - create each file for different css aspects
    - imported that in index.css
    - set up connect between app and css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import 'normalize.css/normalize.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import './styles/index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
