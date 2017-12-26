4ae085e3c394d8ef44e23703f949f1b487a3e5d8
kailinc <kailinc@bu.edu>
2017-12-26 10:55:53 +0700

    Finished SignIn component
    - integrated react-bootstrap
    - got the styling
    - created new file for SignIn

---
+++ src/Components/SignIn.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { FormControl, FormGroup, Col, Form, ControlLabel, Checkbox, Button } from 'react-bootstrap';
+
+class SignIn extends Component {
+  render(){
+    console.log('SignUp: this is user ', this.props.user)
+    return(
+      <Form horizontal>
+        <FormGroup controlId="formHorizontalEmail">
+          <Col componentClass={ControlLabel} sm={2}>
+            Email
+          </Col>
+          <Col sm={10}>
+            <FormControl type="email" placeholder="Email" />
+          </Col>
+        </FormGroup>
+
+        <FormGroup controlId="formHorizontalPassword">
+          <Col componentClass={ControlLabel} sm={2}>
+            Password
+          </Col>
+          <Col sm={10}>
+            <FormControl type="password" placeholder="Password" />
+          </Col>
+        </FormGroup>
+
+        <FormGroup>
+          <Col smOffset={2} sm={10}>
+            <Checkbox>Remember me</Checkbox>
+          </Col>
+        </FormGroup>
+
+        <FormGroup>
+          <Col smOffset={2} sm={10}>
+            <Button type="submit">
+              Sign in
+            </Button>
+          </Col>
+        </FormGroup>
+      </Form>
+    )
+  }
+}
+
+export default SignIn;
