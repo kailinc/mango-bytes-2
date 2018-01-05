72c9080dba6a457d1e026cac67e34a4c023c2047
kailinc <kailinc@bu.edu>
2018-01-05 13:20:39 +0700

    Added <Aside />
    - added imgs for home page
    - styled homepage
    - added hover effect for img

---
+++ src/Components/Aside.js
@@ -0,0 +1,23 @@
+import React, { Component  } from 'react';
+import { Link } from 'react-router-dom';
+
+class Aside extends Component {
+  render(){
+    return(
+      <div>
+        <div className='aside-img-div'>
+          <img className="asideImg" src={this.props.img} />
+          <div class="middle">
+            <Link className='goal' to='/'>{this.props.hoverMsg}</Link>
+          </div>
+        </div>
+        <div className='asideText'>
+          <h3>{this.props.headline}</h3>
+          <p>{this.props.message}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Aside;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,10 @@
 import React, { Component  } from 'react';
+
 import Greeting from './Greeting';
 import Banner from './Banner';
+import Aside from './Aside';
+import riseImg from '../assets/rise-and-code.jpg';
+import hackathonImg from '../assets/hackathon.jpg';
 
 class Home extends Component {
   render(props){
@@ -8,6 +12,18 @@ class Home extends Component {
       <div>
         <Greeting />
         <Banner />
+        <div className="aside-container">
+          <Aside
+            img={riseImg}
+            headline='RISE AND CODE'
+            message='The best way to start the day is with coffee, a computer, and good music.'
+            hoverMsg='SHOP NOW' />
+          <Aside
+            img={hackathonImg}
+            headline='HACKING AWAY'
+            message='There is no better way to learn a new technology than to build with that technology.'
+            hoverMsg='EXPLORE' />
+        </div>
       </div>
     )
   }
