fcff9ecf27c4115bf2526cce5f93784721f72b94
kailinc <kailinc@bu.edu>
2018-01-12 19:48:43 +0700

    Created <Item />
    - added positioning css
    - will render item info and price

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
 import Filters from './Filters';
+import Item from './Item';
 
 class Feed extends Component {
   render() {
+    const items = this.props.items.map(() => <Item />)
     return(
       <div className='feed-container'>
         <Filters />
         <div className='items'>
-
+          { items }
         </div>
       </div>
     )
---
+++ src/Components/Item.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Item extends Component {
+  render() {
+    return(
+      <div className='item'>
+        <h1>This is item</h1>
+      </div>
+    )
+  }
+}
+
+export default Item;
