98980612616c1bb8fe96bd77ebffc5584cf10516
kailinc <kailinc@bu.edu>
2018-01-17 14:43:41 -0500

    Finished Basic Styling for <ItemPage />
    - img on the left
    - description of price, item, attributes on the right

--- src/Components/ItemPage.js
+++ src/Components/ItemPage.js
@@ -1,12 +1,15 @@
 import React, { Component  } from 'react';
 
 import API from '../API';
+import Headline from './Headline';
+import Aside from './Aside';
 
 class ItemPage extends Component {
   constructor(){
     super()
     this.state = {
-      item: {}
+      item: {},
+      quantity: 0
     }
   }
 
@@ -23,10 +26,35 @@ class ItemPage extends Component {
   }
 
   render(){
-    console.log('ItemPage this is match', )
+    const attributesArray = [{javascript: 10},{ python: 10}, {css: 10}]
+    const attributes = attributesArray.map((attribute, index)=> <li
+      key={index}
+      className='attributes'>
+      {Object.keys(attribute)[0]}: <span className='increase-pts'>+{Object.values(attribute)[0]}</span>
+    </li>)
+
     return(
-      <div>
-        Item Page: Item Id = {this.props.match.params.id}
+      <div className='itemPage'>
+        <div className='itemProfile'>
+          <div className='itemImgs'>
+            <img className='mainImg' src={this.state.item.img} alt='item' />
+          </div>
+          <div className='itemInfo'>
+            <h2>{this.state.item.name}</h2>
+            <p>${this.state.item.basePrice}</p>
+            <p>{this.state.item.des}</p>
+            <ul>
+              <li>DevCred: {this.state.item.devCred}</li>
+              { attributes }
+            </ul>
+            <div className='items'>
+              <button>+</button>
+              <p>{this.state.quantity}</p>
+              <button>+</button>
+            </div>
+          </div>
+        </div>
+
       </div>
     )
   }
