9ee303f0ff5d0a38b736f08c22b831f0c7d633f3
kailinc <kailinc@bu.edu>
2018-01-20 10:39:19 -0500

    Created createItem() for <ItemForm/>
     - function is used when there isn't a function
     - created mapStateToProps to get store from redux

--- src/Components/ItemForm.js
+++ src/Components/ItemForm.js
@@ -16,6 +16,27 @@ class ItemForm extends Component {
     this.setState((prevState) => ({
       quantity: prevState.quantity + 1
     }))
+    // get the cart
+    // push the item to cart
+    // if duplicate, increase the quantity
+    // dispatch it
+    const itemsArray = this.props.cart.items
+
+    let cart
+    if (itemsArray.length === 0) {
+      cart = createCart(this.state.item)
+    } else {
+
+    }
+
+    // for (let i = 0; i < items.length; i++) {
+    //   if (items[i].id === this.state.item.id) {
+    //     items[i].quantity += 1
+    //   } else {
+    //     items.push(this.state.item)
+    //   }
+    // }
+    console.log('itemForm: this is items after add ', cart)
   }
 
   minus() {
@@ -36,4 +57,16 @@ class ItemForm extends Component {
   }
 }
 
-export default connect()(ItemForm);
+const mapStateToProps = (state, props) => {
+  return {
+    user: state.user,
+    cart: state.cart
+  };
+};
+
+const createCart = function(item) {
+  item.quantity = 1
+  return [item]
+}
+
+export default connect(mapStateToProps)(ItemForm);
