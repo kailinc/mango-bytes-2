7875d52bdea4b7ca789b177536b3409c2850a04f
kailinc <kailinc@bu.edu>
2018-01-30 19:39:53 -0500

    Finished Styling <Footer />
    - used Nike footer as an inspiration
    - created black footer with h3 and p

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,8 +3,33 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return(
-      <div>
-        This is Footer
+      <div className="footer">
+        <div>
+          <h3>GIFT CARDS</h3>
+          <h3>STUDENT DISCOUNTS</h3>
+          <h3>MILITARY DISCOUNTS</h3>
+          <h3>FIND A STORE</h3>
+          <h3>SIGN UP FOR EMAIL</h3>
+          <h3>BECOME A MEMBER</h3>
+          <h3>PROVIDE FEEDBACK</h3>
+        </div>
+        <div>
+          <h3>CONTACT US</h3>
+          <p>Order Status</p>
+          <p>Shipping and Delivery</p>
+          <p>Returns</p>
+          <p>Payment Options</p>
+          <p>Contact Us</p>
+        </div>
+        <div>
+          <h3>ABOUT MANGO-BYTES 2.0</h3>
+          <p>News</p>
+          <p>Careers</p>
+          <p>Converse Culture & Careers</p>
+          <p>Investors</p>
+          <p>Sustainable Innovation</p>
+          <p>CA Supplychain Act</p>
+        </div>
       </div>
     )
   }
