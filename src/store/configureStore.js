c674e17b16b662ebf6c48a6f25e259743df263b1
kailinc <kailinc@bu.edu>
2018-01-30 21:15:06 -0500

    Saved and Load state to localStorage
    - state for user and cart will persist if there is a page refresh
    - followed a video on Google to set it up
    - the example was a bit different in that it only had 1 reducer, so it
      did not combine reducers. This is what my situation was.
    - after looking at redux doc, it hinted how to pass in state from server
      to createStore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,12 +2,13 @@ import { createStore, combineReducers } from 'redux';
 import userReducer from '../reducers/user';
 import cartReducer from '../reducers/cart';
 
-export default () => {
+export default (persistedState) => {
   const store = createStore(
     combineReducers({
       user: userReducer,
       cart: cartReducer
-    })
+    }),
+    persistedState
   );
 
   return store;
