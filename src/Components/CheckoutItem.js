3acc0755f054837bd27442432e29cf69e8e561c9
kailinc <kailinc@bu.edu>
2018-02-09 21:38:52 -0500

    Added number conversion into dollar form
    - used stack over flow for help
    - used regex

--- src/Components/CheckoutItem.js
+++ src/Components/CheckoutItem.js
@@ -8,22 +8,25 @@ class CheckoutItem extends Component {
     }
   }
   render() {
+    console.log('checkoutItem: this is item ', this.state.item)
     return(
       <div className="item-hr">
         <div>
           <img src={this.state.item.img} />
         </div>
         <div>
-          <p>{this.state.item.description}</p>
+          <p>{this.state.item.name}</p>
+          <p>{this.state.item.devCred}</p>
+          <p>{this.state.item.attributes}</p>
         </div>
         <div>
-          <p>{this.state.item.price}</p>
+          <p>${this.state.item.basePrice.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')}</p>
         </div>
         <div>
           <p>{this.state.item.quantity}</p>
         </div>
         <div>
-          <p>$0.00</p>
+          <p>${(this.state.item.quantity * this.state.item.basePrice).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')}</p>
         </div>
       </div>
     )
