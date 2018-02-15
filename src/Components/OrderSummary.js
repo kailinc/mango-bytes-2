d9750622095b5374aa5bcceef352abeb6f24d84c
kailinc <kailinc@bu.edu>
2018-02-15 11:19:27 -0500

    Created variables for storing prices, tax,
    - did this because discount, tax, and product total relied on each other
      for their value
    - tried to save it in state, but mapToState does not automatically
      update state according to store. MaptoState just gives us access to
      the newest changes in store

--- src/Components/OrderSummary.js
+++ src/Components/OrderSummary.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux';
 
 import { getTotal, convertToDollars } from '../helpers/cart';
 
 class OrderSummary extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      discountCode: '',
+      discountValue: 0
+    }
+  }
 
   render() {
+    const productTotal = getTotal(this.props.cart.items)
+    const subTotal = productTotal - this.state.discountValue
+    const taxes = subTotal * 0.0625
+    const total = subTotal + taxes
+
     return (
       <div className="order-col">
         <div className="summary-content">
@@ -26,7 +37,7 @@ class OrderSummary extends Component {
           </div>
           <div className="summary-row">
             <p>PRODUCT TOTAL:</p>
-            <p>{convertToDollars(getTotal(this.props.cart.items))}</p>
+            <p>{convertToDollars(productTotal)}</p>
           </div>
           <div className="summary-row">
             <p>DELIVERY AND HANDLING:</p>
@@ -34,7 +45,7 @@ class OrderSummary extends Component {
           </div>
           <div className="summary-row">
             <p>ESTIMATED TAXES:</p>
-            <p>$0.00</p>
+            <p>{convertToDollars(taxes)}</p>
           </div>
           <div className="summary-row">
             <p>DISCOUNTS:</p>
@@ -42,7 +53,7 @@ class OrderSummary extends Component {
           </div>
           <div className="summary-row">
             <p>TOTAL:</p>
-            <p>$0.00</p>
+            <p>{convertToDollars(total)}</p>
           </div>
         <div className="summary-row">
           <form className="checkout-form">
@@ -61,7 +72,7 @@ class OrderSummary extends Component {
   }
 }
 
-const mapStateToProps = (state, props) => {
+const mapStateToProps = (state) => {
   return {
     cart: state.cart
   };
