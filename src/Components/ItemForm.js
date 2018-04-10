78862af8ae237843fdd9e981dcef2db3c687c450
kailinc <kailinc@bu.edu>
2018-04-09 22:05:12 -0400

    Fixed Item issue in ItemForm
    - in <Shopping> item is passed in but don't have item_id
    - in <Checkout> item is passed in with item_id
    - this getQuantity not work for both cases

--- src/Components/ItemForm.js
+++ src/Components/ItemForm.js
@@ -7,21 +7,32 @@ import { itemExists, getQuantity, seperateAttr } from '../helpers/cart';
 class ItemForm extends Component {
   constructor(props){
     super(props)
+    this.state = {
+      item: {}
+    }
     this.addOne = this.addOne.bind(this)
     this.minusOne = this.minusOne.bind(this)
     this.formatItem = this.formatItem.bind(this)
     this.formatAttributes = this.formatAttributes.bind(this)
   }
 
+  componentDidMount() {
+    let item = this.formatItem();
+    this.setState({
+      item: item
+    })
+  }
+
+
   formatItem() {
     return {
-      item_id: this.props.item.id,
+      item_id: this.props.item.id || this.props.item.item_id,
       name: this.props.item.name,
       devCred: this.props.item.devCred,
       basePrice: this.props.item.basePrice,
       attributes: this.props.item.attributes,
       img: this.props.item.img,
-      quantity: 0
+      quantity: 1
     }
   }
 
@@ -36,29 +47,26 @@ class ItemForm extends Component {
   }
 
   addOne() {
-    let curItem = this.formatItem();
-    curItem.quantity = 1;
     let attributes = this.formatAttributes();
 
     if (this.props.cart.items.length === 0 ) {
-      this.props.dispatch(newCart(curItem))
+      this.props.dispatch(newCart(this.state.item))
       this.props.dispatch(newAttributes(attributes))
-    } else if (itemExists(this.props.cart.items, curItem.item_id)) {
-      this.props.dispatch(updateQuantity(curItem.item_id, 1))
+    } else if (itemExists(this.props.cart.items, this.state.item.item_id)) {
+      this.props.dispatch(updateQuantity(this.state.item.item_id, 1))
       this.props.dispatch(updateAttributes(attributes, 1))
     } else {
-      this.props.dispatch(addItem(curItem))
+      this.props.dispatch(addItem(this.state.item))
       this.props.dispatch(updateAttributes(attributes, 1));
     }
   }
 
   minusOne() {
-    let curItem = this.formatItem();
     let attributes = this.formatAttributes();
 
     if (this.props.cart.items.length > 0) {
-      if (itemExists(this.props.cart.items, curItem.item_id)) {
-        this.props.dispatch(updateQuantity(curItem.item_id, -1))
+      if (itemExists(this.props.cart.items, this.state.item.item_id)) {
+        this.props.dispatch(updateQuantity(this.state.item.item_id, -1))
         this.props.dispatch(updateAttributes(attributes, -1))
       }
     }
@@ -67,7 +75,7 @@ class ItemForm extends Component {
     return(
       <div className='items'>
         <button onClick={this.addOne}>+</button>
-        <p>{getQuantity(this.props.cart.items, this.props.item.id)}</p>
+        <p>{getQuantity(this.props.cart.items, this.state.item.item_id)}</p>
         <button onClick={this.minusOne}>-</button>
       </div>
     )
