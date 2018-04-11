6c0719468fb61eff6208f1ce2ffd3ba8129bc10a
kailinc <kailinc@bu.edu>
2018-04-11 10:38:25 -0400

    Added HTML for delivery information form
    - trying to replicate adidas's input animation and css for forms

--- src/Components/Shipping.js
+++ src/Components/Shipping.js
@@ -15,10 +15,33 @@ class Shipping extends Component {
           <h3>Step 2. Shipping Information</h3>
         </div>
         <div className="cart-table">
-          <p>fill out some information</p>
+          <h3>Delivery Method</h3>
+          <p>Please allow 2-3 days for processing</p>
+          <h3>Shipping Address</h3>
           <form>
+            <p>First Name</p>
             <input/>
+            <p>Last Name</p>
+            <input/>
+            <p>Street Address, PO Box</p>
+            <input/>
+            <p>City/Town</p>
+            <input/>
+            <p>State</p>
+            <input/>
+            <p>Zip Code</p>
+            <input/>
+            <p>Country</p>
+            <input/>
+
+            <p>Phone Number</p>
+            <input/>
+            <p>Email</p>
+
           </form>
+
+          <h3>Delivery Method</h3>
+          <p>FedEx (3-5 Day Delivery)</p>
         </div>
         <button onClick={this.props.handleAdvance}>Payment</button>
       </div>
