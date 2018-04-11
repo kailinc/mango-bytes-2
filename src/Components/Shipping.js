76dd2cdbd021b2175029b9ee23e455bb2be6eb02
kailinc <kailinc@bu.edu>
2018-04-11 12:03:57 -0400

    Added CSS for input field
    - copied adidas input field

--- src/Components/Shipping.js
+++ src/Components/Shipping.js
@@ -8,6 +8,47 @@ class Shipping extends Component {
     }
   }
 
+  // render() {
+  //   return (
+  //     <div className="cart-col">
+  //       <div className="cart-header">
+  //         <h3>Step 2. Shipping Information</h3>
+  //       </div>
+  //       <div className="cart-table">
+  //         <h3>Delivery Method</h3>
+  //         <label>Please allow 2-3 days for processing</label>
+  //         <h3>Shipping Address</h3>
+  //         <form>
+  //           <div>
+  //             <input/>
+  //             <label>First Name</label>
+  //           </div>
+  //           <label>Last Name</label>
+  //           <input/>
+  //           <label>Street Address, PO Box</label>
+  //           <input/>
+  //           <label>City/Town</label>
+  //           <input/>
+  //           <label>State</label>
+  //           <input/>
+  //           <label>Zip Code</label>
+  //           <input/>
+  //           <label>Country</label>
+  //           <input/>
+  //
+  //           <label>Phone Number</label>
+  //           <input/>
+  //           <label>Email</label>
+  //
+  //         </form>
+  //
+  //         <h3>Delivery Method</h3>
+  //         <label>FedEx (3-5 Day Delivery)</label>
+  //       </div>
+  //       <button onClick={this.props.handleAdvance}>Payment</button>
+  //     </div>
+  //   )
+  // }
   render() {
     return (
       <div className="cart-col">
@@ -16,32 +57,19 @@ class Shipping extends Component {
         </div>
         <div className="cart-table">
           <h3>Delivery Method</h3>
-          <p>Please allow 2-3 days for processing</p>
+          <label>Please allow 2-3 days for processing</label>
           <h3>Shipping Address</h3>
           <form>
-            <p>First Name</p>
-            <input/>
-            <p>Last Name</p>
-            <input/>
-            <p>Street Address, PO Box</p>
-            <input/>
-            <p>City/Town</p>
-            <input/>
-            <p>State</p>
-            <input/>
-            <p>Zip Code</p>
-            <input/>
-            <p>Country</p>
-            <input/>
 
-            <p>Phone Number</p>
-            <input/>
-            <p>Email</p>
+            <div className="inputBox">
+              <input className="inputField half"/>
+              <label>FIRST NAME</label>
+            </div>
 
           </form>
 
           <h3>Delivery Method</h3>
-          <p>FedEx (3-5 Day Delivery)</p>
+          <label>FedEx (3-5 Day Delivery)</label>
         </div>
         <button onClick={this.props.handleAdvance}>Payment</button>
       </div>
