887a869efed6278f934bb22e34bbd9e5e23f82c7
kailinc <kailinc@bu.edu>
2018-04-16 10:39:46 -0400

    Saved user email to react-redux - forgot to have a field for email

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,10 +1,11 @@
-export const signIn = ({id, firstName, lastName, coderName, token, skills, powers, devCred,JavaScript, Python, C, Css, Go, Html, Java, Ruby, Angular, React, BootStrap, Django, Ember, MongoDB, Sql, Node, Rails}, fourScreen = {}) => ({
+export const signIn = ({id, firstName, lastName, coderName, token, skills, powers, devCred,JavaScript, Python, C, Css, Go, Html, Java, Ruby, Angular, React, BootStrap, Django, Ember, MongoDB, Sql, Node, Rails, email}, fourScreen = {}) => ({
     type: 'SIGN_IN',
     user: {
       id,
       firstName,
       lastName,
       coderName,
+      email,
       token,
       skills,
       powers,
