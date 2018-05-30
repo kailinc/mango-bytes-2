ad7e8373fc0f76e5bacc8d1f77bcd36ae6d014d1
kailinc <kailinc@bu.edu>
2018-05-30 15:51:54 +0700

    applied className increase to attribtue label  - this will make the label of the attribute flash only if an item    added to cart has that attribute

--- src/Components/Skill.js
+++ src/Components/Skill.js
@@ -8,9 +8,10 @@ class Skill extends Component {
   render(){
     const exp = this.props.user.token ? this.props.user[this.props.skill] : 0
     const num = this.props.cart.attributes[this.props.skill] || 0
+    const flash = this.props.cart.attributes[this.props.skill] > 0 ? 'increase' : ''
     return(
       <div>
-        <p>{this.props.skill}: {exp} <Attribute num={num}/></p>
+        <p className={flash}>{this.props.skill}: {exp} <Attribute num={num}/></p>
       </div>
     )
   }
