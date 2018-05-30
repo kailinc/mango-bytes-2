bbacf0d95fad6b3f8a691fd3e44251d1d53c4c1f
kailinc <kailinc@bu.edu>
2018-05-30 11:08:53 +0700

    Refactored code for filter helper functions - used ternary statements instead of if, else statements

--- src/helpers/filter.js
+++ src/helpers/filter.js
@@ -1,43 +1,17 @@
 export const comparePrice = function (a,b) {
-  if (a.basePrice === b.basePrice) {
-    return 0
-  } else if (a.basePrice > b.basePrice) {
-    return -1
-  } else {
-    return 1
-  }
+  return a.basePrice <= b.basePrice ? 1 : -1;
 }
 
 export const compareValue = function (a,b) {
-  const valA = a.attributes.length
-  const valB = b.attributes.length
-  if (valA === valB) {
-    return 0
-  } else if (valA > valB) {
-    return -1
-  } else {
-    return 1
-  }
+  return a.attributes.length <= b.attributes.length ? 1 : -1;
 }
 
 export const compareDevCred = function (a,b) {
-  if (a.devCred === b.devCred) {
-    return 0
-  } else if (a.devCred > b.devCred) {
-    return -1
-  } else {
-    return 1
-  }
+  return a.devCred <= b.devCred ? 1 : -1;
 }
 
 export const compareDate = function (a,b) {
   const dateA = new Date(a.createdAt)
   const dateB = new Date(b.createdAt)
-  if (dateA.getTime() === dateB.getTime()) {
-    return 0
-  } else if (dateA.getTime() > dateB.getTime()) {
-    return -1
-  } else {
-    return 1
-  }
+  return dateA.getTime() <= dateB.getTime() ? 1 : -1;
 }
