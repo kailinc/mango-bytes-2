a71c81305564b31aa27a82e18f0bad1817c02184
kailinc <kailinc@bu.edu>
2018-05-30 14:24:23 +0700

    Created msg for popup - added greeting text - was gonna create a component for this, but window.alert() is exists

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,10 @@ import Aside from '../Components/Aside';
 import riseImg from '../assets/rise-and-code.jpg';
 import hackathonImg from '../assets/hackathon.jpg';
 
+
 class Home extends Component {
   render(props){
+    window.alert("Hello! Welcome to Mango Bytes 2.0. This is a make believe e-commerce website for developers. There are items on this website you can purchase that will \"increase\" your coding ability. You can purchase experience in programming languages, stickers, swag, and drones on this website. Use the credit card number 4242-4242-4242 and any number for other fields to purchase items on this website. If you enjoyed this project please give it a star on GitHub.");
     return(
       <div>
         <Greeting />
