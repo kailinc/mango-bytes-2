baf4e15590e93f2aab08463ccff0da424259a3ee
kailinc <kailinc@bu.edu>
2018-05-30 15:47:36 +0700

    Added css color for adding attributes - created an animation for flashing different types of green

--- src/Components/Attribute.js
+++ src/Components/Attribute.js
@@ -3,7 +3,7 @@ import React, { Component  } from 'react';
 class Attribute extends Component {
   render(){
     if (this.props.num > 0) {
-      return (<span className="flash">+{this.props.num}</span>)
+      return (<span className="increase">+{this.props.num}</span>)
     } else {
       return (<span></span>)
     }
