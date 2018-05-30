73a1e92495713b9bb53debfa760f2b7f837fa6d5
kailinc <kailinc@bu.edu>
2018-05-30 15:35:17 +0700

    Added new color for side profile - used black theme instead from ordersummary - this looks alot better

--- src/Components/Attribute.js
+++ src/Components/Attribute.js
@@ -3,7 +3,7 @@ import React, { Component  } from 'react';
 class Attribute extends Component {
   render(){
     if (this.props.num > 0) {
-      return (<span>+{this.props.num}</span>)
+      return (<span className="flash">+{this.props.num}</span>)
     } else {
       return (<span></span>)
     }
