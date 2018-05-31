a4ee9d7304ad44450486aff9d4a1025260bd44db
kailinc <kailinc@bu.edu>
2018-05-31 10:34:48 +0700

    Added basic styling

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
+import notFound from '../assets/notFound.jpg';
+
 class NotFoundPage extends Component {
   render() {
     return (
-      <div>
-        <h1>The page you are looking for is not here.</h1>
+      <div className="notFoundCon">
+        <div className="errorInfo">
+          <h1>Even the things we love break sometimes</h1>
+          <h3>Thanks for your patience while we put the pieces back together.</h3>
+          <p>In the meantime, you can...</p>
+          <ul className="redirect">
+            <li>Shop through AWESOME items on this page.</li>
+            <li>Give me a start on GitHub.</li>
+            <li>Donate Bitcoin/Ethereum/Litecoin to me!</li>
+          </ul>
+        </div>
+        <div>
+          <img src={notFound}/>
+        </div>
       </div>
     )
   }
