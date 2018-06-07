87ed53e3bcb7a3262a3727297bfcae48db0734cd
kailinc <kailinc@bu.edu>
2018-06-07 22:14:14 +0700

    added super power fields to user profile state  - did this to keep track of img urls

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,17 +3,41 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { getNum } from '../helpers/attributes';
+import itemAPI from '../API/item';
 
 class Profile extends Component {
   constructor(props) {
     super(props)
     this.state = {
       cartId: '',
+      FireFingers: '',
+      PestControlla: '',
+      DocKing: '',
+      CoffeeAngel: '',
+      TypNO: '',
+      WalkingGoogle: ''
     }
   }
 
+  componentWillMount(){
+    itemAPI.getItems()
+      .then((response) => response.data.items.filter((cur) => cur.category === 'superpowers'))
+      .then((powers) => {
+        powers.forEach((cur) => {
+          const name = cur.name
+          this.setState({
+            [name]: cur.img
+          })
+        })
+      })
+      .catch((error)=> {
+        console.log(error)
+      })
+  }
+
   render(){
-    const attributes = Object.keys(this.props.user).filter((cur) => cur !== 'id' && cur !== 'token' && cur !== 'devCred' && cur !== 'email' && cur !== 'fourScreen' && cur !== 'powers' && cur !== 'firstName' && cur !== 'coderName' && cur !== 'lastName' ).map( (cur, index) => {
+    const attributes = ['JavaScript', 'BootStrap', 'Html','Css', 'Python', 'Django', 'Ruby', 'Rails', 'C', 'Java', 'Go', 'Ember', 'Node', 'MongoDB', 'Sql', 'Angular', 'React' ]
+    const attributesContent = attributes.map((cur, index) => {
       const level = this.props.user[cur]
       const tileNum = getNum(level)
       let glow = "Glow"
@@ -21,12 +45,21 @@ class Profile extends Component {
         glow = " gold" + glow
       }
       return (
-          <div className="badgeContainer">
+          <div key={index} className="badgeContainer">
             <span className={ glow+ " tile tile" +tileNum}>
             </span>
             <p className={"tileLabel label" +tileNum}>{cur}</p>
             <p className={"label" + tileNum}>{level}</p>
           </div>)
+    })
+    const powers = ['FireFingers', 'PestControlla', 'DocKing', 'CoffeeAngel', 'TypNO', 'WalkingGoogle']
+    let powersContent = powers.map((cur, index) => {
+      return (
+        <div key={index} className='superPowerContainer'>
+          <h3 className='spLabel'>{cur}</h3>
+          <img src={this.state[cur]}/>
+        </div>
+      )
     })
     return(
       <div>
@@ -44,22 +77,11 @@ class Profile extends Component {
         </div>
         <h1 className="profHeader">Attributes</h1>
         <div className="tiles">
-          {attributes}
+          {attributesContent}
         </div>
         <h1 className="profHeader">Super Power</h1>
         <div className="tiles">
-          <div className='superPowerContainer'>
-            <h3 className='spLabel'>Super Power Name</h3>
-            <p className="spText">long funny description about the super power</p>
-          </div>
-          <div className='superPowerContainer'></div>
-          <div className='superPowerContainer'></div>
-          <div className='superPowerContainer'></div>
-          <div className='superPowerContainer'></div>
-          <div className='superPowerContainer'></div>
-          <div className='superPowerContainer'></div>
-          <div className='superPowerContainer'></div>
-          <div className='superPowerContainer'></div>
+          {powersContent}
         </div>
       </div>
     )
