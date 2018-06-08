6a3c4714cd2456ae309627836889f3e3b426d457
kailinc <kailinc@bu.edu>
2018-06-08 09:58:48 +0700

    Added 2 functions to SPForm - purchase() will add the super power to the cart if the user doesn't   already have that super power or if the item is not in the cart - formatItem() just formats the item to only extract the essential info

--- src/Components/SPForm.js
+++ src/Components/SPForm.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
+
 import { connect } from 'react-redux';
 
+import { addItem } from '../actions/cart';
+
 class SPForm extends Component {
+  constructor() {
+    super()
+    this.purchase = this.purchase.bind(this)
+    this.formatItem = this.formatItem.bind(this)
+  }
+
+  formatItem() {
+    return {
+      item_id: this.props.item.id,
+      name: this.props.item.name,
+      devCred: this.props.item.devCred,
+      basePrice: this.props.item.basePrice,
+      attributes: [],
+      img: this.props.item.img,
+      quantity: 1,
+      category: this.props.item.category
+    }
+  }
+
+  purchase() {
+    const powerName = this.props.item.name
+    if (!this.props.user[powerName]) {
+      if (!this.props.cart.items.filter((cur) => cur.name === powerName).length > 0) {
+        const item = this.formatItem();
+        this.props.dispatch(addItem(item))
+      }
+    }
+  }
+
   render(){
     let spClass = "spForm sp" + this.props.pos
     if (this.props.pos === 'Left' || this.props.pos === 'Right') {
@@ -11,7 +43,7 @@ class SPForm extends Component {
       <div className={spClass}>
         <h3>{this.props.item.name}</h3>
         <p>{this.props.item.des}</p>
-        <button className="promo-btn spBtn btn">Buy</button>
+        <button onClick={this.purchase} className="promo-btn spBtn btn">Buy</button>
       </div>
     )
   }
