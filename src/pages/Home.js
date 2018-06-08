f51e993b30e97dbee3f1811416a97b3647285a58
kailinc <kailinc@bu.edu>
2018-06-08 14:03:31 +0700

    Added wakeBackEnd() to Home Page - this is to wake up the backend that is hosted on heroku - currently using free version of heroku which goes asleep after 15min   of inactivity - this will make sure when user first interacts with the page there is   no delay that looks like the website is broken

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,30 @@
 import React, { Component  } from 'react';
 
-import Greeting from '../Components/Greeting';
-import Banner from '../Components/Banner';
-import Aside from '../Components/Aside';
 import riseImg from '../assets/rise-and-code.jpg';
 import hackathonImg from '../assets/hackathon.jpg';
 
+import Greeting from '../Components/Greeting';
+import Banner from '../Components/Banner';
+import Aside from '../Components/Aside';
+
+import userAPI from '../API/user';
 
 class Home extends Component {
+  constructor() {
+    super()
+    this.wakeBackEnd = this.wakeBackEnd.bind(this)
+  }
+
+  componentWillMount() {
+    this.wakeBackEnd()
+  }
+
+  wakeBackEnd() {
+    console.log('waking up backend')
+    userAPI.signIn()
+      .catch((err) => console.log(err))
+  }
+
   render(props){
     window.alert("Hello! Welcome to Mango Bytes 2.0. This is a make believe e-commerce website for developers. There are items on this website you can purchase that will \"increase\" your coding ability. You can purchase experience in programming languages, stickers, swag, and drones on this website. Use the credit card number 4242-4242-4242 and any number for other fields to purchase items on this website. If you enjoyed this project please give it a star on GitHub.");
     return(
