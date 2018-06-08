cadc4edc5bb5779882572974b8b7093792e1e24a
kailinc <kailinc@bu.edu>
2018-06-08 15:09:31 +0700

    Added slogans for vertical and horizontal ad

--- src/Components/HRAd.js
+++ src/Components/HRAd.js
@@ -1,10 +1,29 @@
 import React, { Component  } from 'react';
 
 class HRAd extends Component {
+  constructor() {
+    super()
+    this.state = {
+      msgs: ['For 2 Bitcoin this could be your ad',
+             'Google is your friend',
+             'Are you WEBSCALE bro? #Scalable',
+             'Use Promo Code monGOD99 to get $25 off your purchase',
+             'That is not a bug. Its a feature'],
+      msg: ''
+    }
+  }
+
+  componentWillMount() {
+    let num = Math.floor(Math.random() * this.state.msgs.length)
+    this.setState({
+      msg: this.state.msgs[num]
+    })
+  }
+
   render(){
     return(
       <div className="adCon hrAdCon">
-        <h3>{this.props.msg}</h3>
+        <h3>{this.state.msg}</h3>
       </div>
     )
   }
--- src/Components/VAd.js
+++ src/Components/VAd.js
@@ -1,10 +1,29 @@
 import React, { Component  } from 'react';
 
 class VAd extends Component {
+  constructor() {
+    super()
+    this.state = {
+      msgs: ['Need investors? Just add Blockchain to your company name.',
+             'For 2 Bitcoin this can be your ad',
+             'I go to hackathorns for the free swag.',
+             'Use Promo Code monGOD99 to get $25 off your purchase',
+             'TBH, I use Slack because of the dancing parrots.'],
+      msg: ''
+    }
+  }
+
+  componentWillMount() {
+    let num = Math.floor(Math.random() * this.state.msgs.length)
+    this.setState({
+      msg: this.state.msgs[num]
+    })
+  }
+
   render(){
     return(
       <div className="adCon VAdCon">
-        <p>{this.props.msg}</p>
+        <p>{this.state.msg}</p>
       </div>
     )
   }
